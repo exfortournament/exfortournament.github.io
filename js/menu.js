2455b3ff4c60905ebbeb59d420c0f70c604a8672
Artiom <artiomponomariovcode@gmail.com>
2023-06-03 15:37:56 +0300

    responsiveness

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+
+let hamburger = document.querySelector(".box--hamburger")
+let nav = document.querySelector(".box--nav")
+let body = document.querySelector("body")
+const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+hamburger.addEventListener("click",function (event){
+    hamburger.classList.toggle("isActive")
+    nav.classList.toggle("isVisible")
+    body.classList.toggle("isNoScroll")
+    if (body.classList.contains("isNoScroll") ){
+        body.style.marginRight=scrollbarWidth + 'px'
+    } else {
+        body.style.removeProperty("margin-right")
+    }
+})
